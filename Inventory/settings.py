46ef8c3623a1c2be8ea5a53e4b6963f18f26ac4a
temmy <favouradebose@gmail.com>
2024-11-01 01:53:07 +0100

    update settings

--- Inventory/settings.py
+++ Inventory/settings.py
@@ -156,8 +156,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,  # Number of items per page
 
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-        ...
-    )
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework.authentication.TokenAuthentication',
+    #     ...
+    # )
 }
