a32f8f0f85ac43301302b657a4de901ecc06e7ab
temmy <favouradebose@gmail.com>
2024-11-01 02:22:49 +0100

    update inventory models

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name='products')
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name='products', blank=True)
 
     def __str__(self):
         return self.name
