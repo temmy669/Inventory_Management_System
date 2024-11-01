d6d892377d601ba949ed368c446440e8da1d0ae6
temmy <favouradebose@gmail.com>
2024-11-01 01:37:25 +0100

    update settings

--- Inventory/settings.py
+++ Inventory/settings.py
@@ -31,9 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['inventory-management-system-nmrq.onrender.com']
 
 
 # Application definition
